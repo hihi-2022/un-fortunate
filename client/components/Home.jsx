84b8890f64b02680e958eb911d695578eb6661dc
Shaneel Kumar <shaneelkumar.dev@gmail.com>
2022-11-04 10:09:32 +1300

    add style modules to project

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Tilt from 'react-parallax-tilt'
+import style from '../styles/Home.module.css'
 
 function Home() {
   return (
-    <div className="fortune-room">
-      <div className="teller">
+    <div className={style.fortune_room}>
+      <div className={style.teller}>
         <Tilt>
           <img src="/fortune-teller.jpeg" alt="fortune teller" />
         </Tilt>
