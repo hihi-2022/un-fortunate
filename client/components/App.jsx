453781f04fe27c1c3c059bc5ed572d2f1c846558
Alyssa Barbieri <19173672+alyssa-barbieri@users.noreply.github.com>
2022-11-04 10:52:51 +1300

    add fortune cookie component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import FortuneCookie from './FortuneCookie'
 
 import Home from './Home'
 
 function App() {
   return (
     <>
-
       <h1>Un-fortunate</h1>
       <p>A series of D: events</p>
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fortune-cookie" element={<FortuneCookie />} />
+      </Routes>
     </>
   )
 }
