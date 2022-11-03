6b2c52bfee35c27be9d7e3c0f1edb1b22371a7cc
Shaneel Kumar <shaneelkumar.dev@gmail.com>
2022-11-04 09:29:59 +1300

    setup home page and router

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,17 +2,13 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './Home'
-import TiltDemo from './TiltDemo'
 
 function App() {
   return (
     <>
-      {/* <h1>Un-fortunate</h1>
-      <p>A series of D: events</p> */}
-
-      <TiltDemo />
-
-      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Tilt from 'react-parallax-tilt'
 
 function Home() {
   return (
-    <>
-    <h1>HOME PAGE WORKS</h1>
-    </>
+    <div className="fortune-room">
+      <div className="teller">
+        <Tilt>
+          <img src="/fortune-teller.jpeg" alt="fortune teller" />
+        </Tilt>
+      </div>
+    </div>
   )
 }
-
 export default Home
