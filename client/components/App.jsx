5ae575a28a958590e6c29fe90588877b123ee83d
Shaneel Kumar <shaneelkumar.dev@gmail.com>
2022-11-04 08:49:28 +1300

    add home page background and fortune teller w/ tilt

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,18 +2,17 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './Home'
+import TiltDemo from './TiltDemo'
 
 function App() {
   return (
     <>
+      {/* <h1>Un-fortunate</h1>
+      <p>A series of D: events</p> */}
 
-      <h1>Un-fortunate</h1>
-      <p>A series of D: events</p>
+      <TiltDemo />
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-
+      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
     </>
   )
 }
