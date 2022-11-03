d69b3a69ebba5ae47a0525bbbb858a4135d41d32
Eben Serfontein <ebenserf@gmail.com>
2022-11-04 11:15:22 +1300

    tarot card styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './Home'
+import Card from './Card'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Cards" element={<Card />} />
       </Routes>
     </>
   )
--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Tilt from 'react-parallax-tilt'
+import style from '../styles/Card.module.css'
 
 function Card() {
   return (
-    <>
-      <img src="./public/images/tarot-test.png" alt="tarot_image" />
-    </>
+    <div className={style.tarot_cards}>
+      <div className={style.tarot_1}>
+        <Tilt>
+          <img src="/fortune-teller.jpeg" alt="fortune teller" />
+        </Tilt>
+      </div>
+      <div className={style.tarot_1}>
+        <Tilt>
+          <img src="/fortune-teller.jpeg" alt="fortune teller" />
+        </Tilt>
+      </div>
+      <div className={style.tarot_1}>
+        <Tilt>
+          <img src="/fortune-teller.jpeg" alt="fortune teller" />
+        </Tilt>
+      </div>
+    </div>
   )
 }
 
