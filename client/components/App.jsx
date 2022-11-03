81fcc4dc391fdd8827fb90bf715c68dff0780042
Eben Serfontein <ebenserf@gmail.com>
2022-11-04 10:16:22 +1300

    card componenet

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,14 +6,12 @@ import Home from './Home'
 function App() {
   return (
     <>
-
       <h1>Un-fortunate</h1>
       <p>A series of D: events</p>
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
---
+++ client/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <>
+      <img src="./public/images/tarot-test.png" alt="tarot_image" />
+    </>
+  )
+}
+
+export default Card
