1b991b5cacfdd077c9bcf6cac9180788685cc7e3
Eben Serfontein <ebenserf@gmail.com>
2022-11-04 14:13:11 +1300

    styling and effects

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/fortune-cookie" element={<FortuneCookie />} />
+        <Route path="/Card" element={<Card />} />
       </Routes>
     </>
   )
